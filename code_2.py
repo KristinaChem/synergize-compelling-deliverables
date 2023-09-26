a12c8e8281e7f40257c96705ddd9e025e6ea45d6
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-09-26 23:47:47 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        }
+
+}
+        for (int item : data) {
+        }
+        int[] data = new int[10];
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+    }
