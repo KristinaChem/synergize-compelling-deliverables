b712384097d85efc9c4e5d5482a343fdab91415e
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-09-28 01:01:19 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+
+    data = generate_random_data()
+    main()
+import random
+    return data
+def main():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    for item in data:
